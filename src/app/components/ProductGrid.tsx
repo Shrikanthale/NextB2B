b359b76d2eebc9ef0e9c92e6783d2cfd62add633
skanthale <shrikant@unada.io>
2025-04-10 18:18:40 +0530

    uodate product page curosr

--- src/app/components/ProductGrid.tsx
+++ src/app/components/ProductGrid.tsx
@@ -16,7 +16,7 @@ export default function ProductGrid({ posts }) {
                     whileHover={{ scale: 1.03 }}
                     whileTap={{ scale: 0.97 }}
                     key={product.slug}
-                    className="bg-white/10 backdrop-blur-2xl border border-white/10 rounded-3xl shadow-2xl overflow-hidden transition-all duration-300 hover:shadow-pink-500/40"
+                    className="bg-white/10 backdrop-blur-2xl border border-white/10 rounded-3xl shadow-2xl overflow-hidden transition-all duration-300 cursor-pointer hover:shadow-pink-500/40"
                 >
                     <Image
                         src={product.img}
@@ -36,7 +36,7 @@ export default function ProductGrid({ posts }) {
                             </span>
                             <Link href={"/cart"} >
                             
-                            <button className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-pink-500 hover:to-yellow-500 text-white font-semibold px-4 py-2 rounded-full shadow-md transition-all ">
+                            <button className="bg-gradient-to-r from-purple-500 to-pink-500 cursor-pointer hover:from-pink-500 hover:to-yellow-500 text-white font-semibold px-4 py-2 rounded-full shadow-md transition-all ">
                                 Add to Cart
                             </button>
                             </Link>
